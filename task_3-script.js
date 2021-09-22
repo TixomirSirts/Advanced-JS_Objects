9773d0b6d6ed166acddad9abb489a5adb447b7d2
TixomirSirts <tihomir.sirts@yandex.ru>
2021-09-22 20:01:51 +0300

    Made Task 3

---
+++ task_3-script.js
@@ -0,0 +1,26 @@
+console.group(`Выполнение задания 3:`);
+
+const emptyObj1 = () => Object.create(null);
+
+let Var_a = emptyObj1();
+console.log(Var_a);
+
+
+// const testObj3_1 = {};
+// const testObj3_2 = Object.create(Object.prototype);
+// const testObj3_3 = Object.create(null);
+
+// console.log(testObj3_1);
+// console.log(testObj3_2);
+// console.log(testObj3_3);
+
+// const emptyObj2 = () => {
+//     let Obj = {};
+//     for(let prop in Obj) {
+//         prop = null
+//     }
+//     return Obj
+// }
+
+// let Var_b = emptyObj2();
+// console.log(Var_b);
